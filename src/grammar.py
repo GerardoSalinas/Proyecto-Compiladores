da8e5983bd025227bc3beb2cc9985515890fb7cf
GerardoSalinas <gerardoaplicano16@gmail.com>
2025-04-11 11:09:11 -0600

    Includes print feature to grammar and LanguageTransformer

--- src/grammar.py
+++ src/grammar.py
@@ -1,23 +1,25 @@
-grammar = """
-
-?assignment: "$" CNAME ":=" value
-?addition: NUMBER "+" NUMBER
-?subtraction: NUMBER "-" NUMBER
-repetition: INT"*"WORD
-
-value: ESCAPED_STRING -> string
-    | NUMBER          -> number
-    | "true"          -> true
-    | "false"         -> false
-    | nil
-
-nil: "NIL"
+expression_grammar = """
+?start: expression
+?expression: arith | imprimir_op
+?imprimir_op: "imprimirResultado" "(" expression ")"
+?arith:   term   | term "+" arith  -> add | term "-" arith      -> sub
+?term:    factor | factor "*" term -> mul | factor "/" term     -> div | factor "%" term -> mod
+?factor:  pow    | "+" factor      -> pos | "-" factor          -> neg
+?pow: call | atom "**" factor -> pow
+?call:    atom   | call trailer
+?atom:    "(" expression ")" | CNAME -> symbol | INT -> integer | FLOAT -> float | ESCAPED_STRING -> literal
+?trailer: "(" arglist ")"
+?arglist: [expression ("," expression)*]
 
 %import common.CNAME
 %import common.NUMBER
-%import common.WS
-%import common.INT
-%import common.WORD
 %import common.ESCAPED_STRING
-%ignore  WS
+%import common.INT
+%import common.FLOAT
+%import common.WORD
+%import common.INT
+%import common.WS
 """
+
+grammar = "\n".join(["%ignore WS"]) + expression_grammar
+# ?pow:     call "**" factor
